599b9a61a7bc697dfaeb2a8c746c25ea7489bc61
shalin999 <shalin999@mail.ru>
2017-10-03 09:32:31 +0300

    new file:   www/index.php

---
+++ www/function.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shalinnout999
+ * Date: 03.10.2017
+ * Time: 9:23
+ */
---
+++ www/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shalinnout999
+ * Date: 02.10.2017
+ * Time: 9:53
+ */
